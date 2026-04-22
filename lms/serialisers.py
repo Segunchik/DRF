6fcc0ccff83f0af7f725d0f3187e763763a28ae9
Sergey Parkhanovich <komrad.segunchik@yandex.ru>
2026-04-22 20:37:27 +0300

    task1 done

--- lms/serialisers.py
+++ lms/serialisers.py
@@ -1,12 +1,19 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from lms.models import Course, Lesson
 
 
 class CourseSerializer(ModelSerializer):
+    lesson_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    @staticmethod
+    def get_lesson_count(self, instance):
+        return instance.lesson.count()
 
 
 class LessonSerializer(ModelSerializer):
